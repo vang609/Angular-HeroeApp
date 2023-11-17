a9b402c1e9b76d36d6bfc5bffbac699c74acc4dd
en-vang <enrique@si.team>
2023-11-16 20:54:44 -0500

    Heroes App completed

---
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -0,0 +1,20 @@
+import { Hero } from './../../interfaces/hero.interface';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-list-page',
+  templateUrl: './list-page.component.html',
+  styles: [],
+})
+export class ListPageComponent implements OnInit {
+  public heroes: Hero[] = [];
+
+  constructor(private heroesService: HeroesService) {}
+
+  ngOnInit(): void {
+    this.heroesService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes));
+  }
+}
